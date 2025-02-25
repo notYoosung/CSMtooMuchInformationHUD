0baec7778ff8001adaa395abaf41a57dc75ad345
notYoosung <notyoosung4901@gmail.com>
2025-02-24 21:21:47 -0600

    tweaks

--- module_lightLevels.lua
+++ module_lightLevels.lua
@@ -1,23 +1,32 @@
--- pointed above, below
+-- pointed above, under
 -- head, feet
 -- mark nodes with light level 0 ? will need particle displays & (frequent?) checks
 -- core.find_nodes_in_area_under_air(pos1, pos2, nodenames)
 
-local get_node_light = core.get_node_light()
+local get_node_light_formatted = function(pos)
+	local node_light = core.get_node_light(pos)
+	if node_light == 0 then
+		return minetest.colorize("#FF5555", "0")
+	else
+		return tostring(node_light)
+	end
+end
 
 local function update(index)
 	local output = ""
 	local pointed_thing = tmi.pointed_thing
 	if pointed_thing then
 		if pointed_thing.above then
-			output = output .. string.format("Pointed Above Light Level: %02i", get_node_light(pointed_thing.above))
+			output = output .. "\n" .. "Pointed Above Light: " ..  get_node_light_formatted(pointed_thing.above)
 		end
-		if pointed_thing.below then
-			output = output .. string.format("Pointed Below Light Level: %02i", get_node_light(pointed_thing.below))
+		if pointed_thing.under then
+			output = output .. "\n" .. "Pointed Below Light: " ..  get_node_light_formatted(pointed_thing.under)
 		end
 	end
-	output = output .. string.format("Head Light Level: %02i", get_node_light(vector.offset(tmi.player_pos, 0, 1, 0)))
-	output = output .. string.format("Feet Light Level: %02i", get_node_light(tmi.player_pos))
+	if tmi.player_pos then
+		output = output .. "\n" .. "Head Light: " ..  get_node_light_formatted(vector.offset(tmi.player_pos, 0, 1, 0))
+		output = output .. "\n" .. "Feet Light: " ..  get_node_light_formatted(tmi.player_pos)
+	end
     
 
 	return output

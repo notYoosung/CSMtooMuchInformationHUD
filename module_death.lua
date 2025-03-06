7f83cee0b342ff481831d44ffe702b286076550f
notYoosung <notyoosung4901@gmail.com>
2025-03-06 16:35:43 -0600

    death check uses on_damage_taken

--- module_death.lua
+++ module_death.lua
@@ -5,25 +5,29 @@
 --wp?
 
 local function onInit()
-    core.register_on_death(function()
-        core.log("error", "died")
-        core.display_chat_message("died")
-        if tmi.player_pos then
-            local death_msg = core.colorize("#E00000",  "You died at " .. core.pos_to_string(tmi.player_pos) .. ".")
-            core.log("error", death_msg)
-            core.display_chat_message(death_msg)
-            tmi.last_death_pos = tmi.player_pos
-            if tmi.player then
-                tmi.player:hud_add({
-                    type = "waypoint",
-                    name = "death",
-                    text = "",
-                    precision = 10,
-                    number = 0x00ff00,
-                    world_pos = tmi.player_pos,
-                    offset = { x = 0, y = 0, z = 0 },
-                    alignment = { x = 0, y = 0 },
-                })
+    --[[core.register_on_death(function()
+    end)--]]
+    core.register_on_damage_taken(function(hp)
+        if tmi.player then
+            if tmi.player:get_hp() - hp < 0 then
+                if tmi.player_pos then
+                    local death_msg = core.colorize("#E00000",
+                        "You died at " .. core.pos_to_string(tmi.player_pos) .. ".")
+                    core.log(death_msg)
+                    tmi.last_death_pos = vector.round(tmi.player_pos)
+                    if tmi.player then
+                        tmi.player:hud_add({
+                            type = "waypoint",
+                            name = "death",
+                            text = "",
+                            precision = 10,
+                            number = 0x00ff00,
+                            world_pos = tmi.last_death_pos,
+                            offset = { x = 0, y = 0, z = 0 },
+                            alignment = { x = 0, y = 0 },
+                        })
+                    end
+                end
             end
         end
     end)
@@ -37,9 +41,9 @@ local function update()
 end
 
 tmi.addModule({
-	id = 'death',
-	title = 'death',
-	value = 'death',
+    id = 'death',
+    title = 'death',
+    value = 'death',
     onInit = onInit,
-	onUpdate = update,
+    onUpdate = update,
 })
